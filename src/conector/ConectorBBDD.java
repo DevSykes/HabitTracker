22b4a1d83ce27e0053d2bfb2004ee12d9be4a3d2
DevSykes <DevSykes@users.noreply.github.com>
2026-04-07 17:56:44 +0200

    refactor: organizar estructura de paquetes

--- src/conector/ConectorBBDD.java
+++ src/conector/ConectorBBDD.java
@@ -1,3 +1,5 @@
+package conector;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,7 @@
+package main;
+
+import conector.ConectorBBDD;
+
 public class Main {
     public static void main(String[] args) {
         ConectorBBDD.conectar();
