33736f623fb28030360c38609bb4d8297ff02fc8
DevSykes <DevSykes@users.noreply.github.com>
2026-04-08 14:36:32 +0200

    feat: usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -15,3 +15,15 @@ public Usuario(String nombre, String email, String password) {
     this.email = email;
     this.password = password;
 }
+
+public int getId() { return id; }
+public void setId(int id) { this.id = id; }
+
+public String getNombre() { return nombre; }
+public void setNombre(String nombre) { this.nombre = nombre; }
+
+public String getEmail() { return email; }
+public void setEmail(String email) { this.email = email; }
+
+public String getPassword() { return password; }
+public void setPassword(String password) { this.password = password; }
