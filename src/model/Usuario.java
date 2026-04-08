88a7366317bc56af3b4f7d3c7ec667b9f748e319
DevSykes <DevSykes@users.noreply.github.com>
2026-04-08 14:38:25 +0200

    feat: implementar clase modelo Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,29 +1,42 @@
 package model;
 
 public class Usuario {
+
+    // atributos privados
+    private int id;
+    private String nombre;
+    private String email;
+    private String password;
+
+    public Usuario() {}
+
+    // nuevos registros
+    public Usuario(String nombre, String email, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+    // getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
-// atributos privados
-private int id;
-private String nombre;
-private String email;
-private String password;
-
-public Usuario() {}
-
-public Usuario(String nombre, String email, String password) {
-    this.nombre = nombre;
-    this.email = email;
-    this.password = password;
-}
-
-public int getId() { return id; }
-public void setId(int id) { this.id = id; }
-
-public String getNombre() { return nombre; }
-public void setNombre(String nombre) { this.nombre = nombre; }
-
-public String getEmail() { return email; }
-public void setEmail(String email) { this.email = email; }
-
-public String getPassword() { return password; }
-public void setPassword(String password) { this.password = password; }
