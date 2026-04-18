0fa0763f24dccff68c19c752e0a8cfb5a120b7b6
DevSykes <DevSykes@users.noreply.github.com>
2026-04-18 21:47:56 +0200

    feat: integrar menu principal en Main y BBDD

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,5 +12,35 @@ public class Main {
         boolean salir = false;
 
         System.out.println("Te damos la bienvenida a HabitTracker");
+
+        while (!salir) {
+            System.out.println("\n--- MENÚ PRINCIPAL ---");
+            System.out.println("1. Gestionar usuarios");
+            System.out.println("2. Mis hábitos");
+            System.out.println("3. Registrar progreso diario");
+            System.out.println("4. Salir");
+            System.out.print("Selecciona una opción: ");
+
+            String opcionStr = sc.nextLine();
+
+            switch (opcionStr) {
+                case "1":
+                    System.out.println("Entrando en gestión de usuarios...");
+                    break;
+                case "2":
+                    System.out.println("Entrando en hábitos...");
+                    break;
+                case "3":
+                    System.out.println("Registrando progreso...");
+                    break;
+                case "4":
+                    System.out.println("Nos vemos");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }
+        sc.close();
     }
 }
