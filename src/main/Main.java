3cc4b42c7b28176b902019036995cbb540a867b9
DevSykes <DevSykes@users.noreply.github.com>
2026-04-18 21:43:02 +0200

    feat: integrar menu principal en Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
 import conector.ConectorBBDD;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Iniciando aplicación...");
         ConectorBBDD.conectar();
+
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+
+        System.out.println("Te damos la bienvenida a HabitTracker");
     }
-}// TODO: Implementar menú principal con Switch/Scanner
-// TODO: Validar login de usuario
-// TODO: Llamar a las funciones de HabitoController
+}
