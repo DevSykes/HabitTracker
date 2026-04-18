e158d33da04c4dac62726e2d1ca5d1816dff32f7
DevSykes <DevSykes@users.noreply.github.com>
2026-04-18 21:33:12 +0200

    feat: atributos privados en registro.java

--- src/model/Registro.java
+++ src/model/Registro.java
@@ -2,3 +2,12 @@ package model;
 
 public class Registro {
 }
+
+public class Registro {
+    // atributos privados
+    private int id;
+    private int idHabito; //  tabla Habitos
+    private LocalDate fecha; // manejar fechas
+    private boolean completado;
+
+    public Registro() {}
