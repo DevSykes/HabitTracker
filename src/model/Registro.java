30a76865f3d121d0c4a7fae27bed29be57b84ea8
DevSykes <DevSykes@users.noreply.github.com>
2026-04-18 21:38:53 +0200

    feat: implementar clase Registro

--- src/model/Registro.java
+++ src/model/Registro.java
@@ -1,4 +1,5 @@
 package model;
+import java.time.LocalDate;
 
 public class Registro {
 }
@@ -18,3 +19,27 @@ public class Registro {
         this.fecha = fecha;
         this.completado = completado;
     }
+
+    //getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getIdHabito() { return idHabito; }
+    public void setIdHabito(int idHabito) { this.idHabito = idHabito; }
+
+    public LocalDate getFecha() { return fecha; }
+    public void setFecha(LocalDate fecha) { this.fecha = fecha; }
+
+    public boolean isCompletado() { return completado; }
+    public void setCompletado(boolean completado) { this.completado = completado; }
+
+
+    @Override
+    public String toString() {
+        return "Registro{" +
+                "idHabito=" + idHabito +
+                ", fecha=" + fecha +
+                ", completado=" + completado +
+                '}';
+    }
+}
